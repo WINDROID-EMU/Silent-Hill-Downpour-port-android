5857ba6394cf40dddfd39cba910d26df60dd7c59
Miceano <pablolabs36@gmail.com>
2026-09-04 12:18:54 -0500

    ci(github-actions): add workflow to build and release Android APK

---
+++ generated/default/codegen.d
@@ -0,0 +1,4 @@
+/media/windroid/SSD\ KING/Silent-Hill-Downpour-port-android/src/../generated/default/codegen.build.stamp: \
+  /media/windroid/SSD\ KING/Silent-Hill-Downpour-port-android/src/../assets/default.xex \
+  /media/windroid/SSD\ KING/Silent-Hill-Downpour-port-android/src/downpour_manifest.toml \
+  /media/windroid/SSD\ KING/Silent-Hill-Downpour-port-android/downpour_config.toml
